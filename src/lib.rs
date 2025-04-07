d38f977d18261578c864085ecd38c3109b1ee6ae
Ali Issaee <rezaa91@hotmail.co.uk>
2025-04-07 21:10:29 +0100

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
 use std::fs;
 
 #[derive(Debug)]
-pub struct CatArgs {
+pub struct Args {
+    pub number: bool,
+    pub show_ends: bool,
+}
+
+impl Args {
+    fn new() -> Args {
+        Args {number: false, show_ends: false}
+    }
+}
+
+#[derive(Debug)]
+pub struct Catr {
     pub files: Vec<String>,
-    pub arguments: Vec<String>,
+    pub arguments: Args,
 }
 
 pub fn execute(args: &Vec<String>) {
@@ -17,17 +29,21 @@ pub fn execute(args: &Vec<String>) {
     }
 }
 
-pub fn parse_args(args: &Vec<String>) -> CatArgs {
-    let mut arguments: Vec<String> = Vec::new();
+pub fn parse_args(args: &Vec<String>) -> Catr {
     let mut files: Vec<String> = Vec::new();
+    let mut arguments = Args::new();
 
     for a in args {
         if a.starts_with('-') {
-            arguments.push(String::from(a));
+            match a.as_str() {
+                "--number" => arguments.number = true,
+                "--show-ends" => arguments.show_ends = true,
+                _ => println!("Invalid argument [{}]", a),
+            }
         } else {
             files.push(String::from(a));
         }
     }
 
-    CatArgs {files, arguments}
+    Catr {files, arguments}
 }
